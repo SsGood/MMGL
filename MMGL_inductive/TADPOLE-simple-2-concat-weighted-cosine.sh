45d9d1425d5e6a29501c23323165a7cf2e0db802
SsGood <38525791+SsGood@users.noreply.github.com>
2022-11-10 15:45:08 +0800

    Add files via upload

---
+++ MMGL_inductive/TADPOLE-simple-2-concat-weighted-cosine.sh
@@ -0,0 +1,2 @@
+python main.py --GC_mode weighted-cosine --MF_mode concat --MP_mode GCN --datname TADPOLE --dropout 0 --lr 0.0055 --mode simple-2 --n_head 2 --n_hidden 18 --nlayer 3 --reg 0.26
+python main.py --GC_mode weighted-cosine --MF_mode concat --MP_mode GCN --datname TADPOLE --dropout 0 --lr 0.0055 --mode simple-2 --n_head 2 --n_hidden 18 --nlayer 3 --reg 0.26
